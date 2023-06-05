7fa47f9275fc327812f2b33eda13808009af8438
Fadoua Marzouk <99365677+fadwamrzk@users.noreply.github.com>
2023-06-05 19:29:34 +0100

    Update all.kt

--- app/src/main/java/com/example/swapevent/fragments/all.kt
+++ app/src/main/java/com/example/swapevent/fragments/all.kt
@@ -17,26 +17,6 @@ class All : Fragment() {
 
         val recyclerVAll = view?.findViewById<RecyclerView>(R.id.recyclerViewEventsAll)
 
-      /*  val eventService= RetrofitInstance.buildEventsService()
-        eventService.getEvent().enqueue(object : Callback<General> {
-
-            override fun onResponse(call: Call<General>, response: Response<General>)
-            {
-                if (response.isSuccessful){
-                    val items=response.body() as General
-
-                    val eventsAdapter = EventsAdapter(items._embedded.events,this@All)
-                    recyclerVAll?.layoutManager = LinearLayoutManager(requireContext())
-                    recyclerVAll?.adapter = eventsAdapter
-                    Log.println(Log.ASSERT, "-------ALL--------", response.body().toString())
-                }
-            }
-
-            override fun onFailure(call: Call<General>, t:Throwable){
-                Toast.makeText(requireContext(),"Server Error", Toast.LENGTH_LONG).show()
-                Log.println(Log.ASSERT, "///////", t.message.toString())
-            }
-        })*/
 
         return inflater.inflate(R.layout.fragment_all, container, false)
     }
